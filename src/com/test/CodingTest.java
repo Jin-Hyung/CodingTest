cc7ca74f758bf65ffd7e30321d9713d3f2928a54
JinHyung Kim <wlsgud4616@naver.com>
2020-04-22 00:58:49 +0900

    Initial commit for moeigosa

---
+++ src/com/test/CodingTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class CodingTest {
+
+	
+	
+	
+	public static void main(String[] args) {
+		CodingTest codingTest = new CodingTest();
+		String res = codingTest.solution();
+		
+		System.out.println(res);
+	}
+
+	public String solution() {
+		return "Hello world";
+	}
+}
