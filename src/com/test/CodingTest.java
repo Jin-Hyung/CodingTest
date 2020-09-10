d0dd13b7293216d87ec5e956f52ca3b82a2d7bce
JinHyung Kim <wlsgud4616@naver.com>
2020-09-10 11:56:32 +0900

    김진형 코딩테스트 공부

--- src/com/test/CodingTest.java
+++ src/com/test/CodingTest.java
@@ -1,18 +1,48 @@
 package com.test;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class CodingTest {
-
-	
-	
-	
 	public static void main(String[] args) {
-		CodingTest codingTest = new CodingTest();
-		String res = codingTest.solution();
-		
-		System.out.println(res);
+	  Scanner sc = new Scanner(System.in);
+	  int n = sc.nextInt();
+	  for (int i=1; i<=n*2-1; i++) {
+	    if (i <= n) {
+  	    for (int j=1; j<=i; j++) {
+  	      System.out.print("*");
+  	    }
+  	    for (int j=n-1; j>=i; j--) {
+  	      System.out.print(" ");
+  	    }
+        for (int j=n-1; j>=i; j--) {
+          System.out.print(" ");
+        }
+        for (int j=1; j<=i; j++) {
+          System.out.print("*");
+        }
+  	    System.out.println();
+	    } else {
+	      for (int j=n; j>(i-n); j--) {
+          System.out.print("*");
+        }
+        for (int j=1; j<=i-n; j++) {
+          System.out.print(" ");
+        }
+        for (int j=1; j<=i-n; j++) {
+          System.out.print(" ");
+        }
+        for (int j=n; j>(i-n); j--) {
+          System.out.print("*");
+        }
+        System.out.println();
+	    }
+	    sc.close();
+	  }
+	  
+	  Stack<Map<String, String>> st = new Stack<>();
+	  
 	}
 
-	public String solution() {
-		return "Hello world";
-	}
 }
